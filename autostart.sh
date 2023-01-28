76db52b3fbb9b31f330e5a6ec8778097e002a415
Lxw0628 <761402785@qq.com>
2023-01-28 14:58:10 +0800

    初次提交

---
+++ autostart.sh
@@ -0,0 +1,36 @@
+#! /usr/bin/sh
+
+sh $DWM/statusbar/statusbar.sh cron
+
+# 通知服务器
+# /usr/lib/notification-daemon-1.0/notification-daemon &
+
+# 适用于DWM的极简主义通知守护程序
+killall dunst
+/usr/bin/dunst &
+
+source ~/.zprofile
+
+xset r rate 250 50
+
+nm-applet &
+
+blueman-applet &
+
+killall cfw
+$HOME/cfw/cfw &
+
+fcitx5 &
+
+killall picom 
+picom --experimental-backends --config $HOME/.config/picom/picom.conf &
+
+# feh --bg-fill /run/media/lxw/数据/图片/电脑壁纸/code4k.png &
+feh --bg-fill --randomize $HOME/Pictures/wallpapers/* &
+
+killall xfce-polkit
+/usr/lib/xfce-polkit/xfce-polkit
+
+xmodmap -e "keycode 108 = Super_L"  # reassign Alt_R to Super_L
+xmodmap -e "remove mod1 = Super_L"  # make sure X keeps it out of the mod1 group
+
