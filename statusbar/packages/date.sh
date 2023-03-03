cc1b6aea069676edbc648cae3b2df4d2f60d9e36
Lxw0628 <761402785@qq.com>
2023-03-03 13:15:23 +0800

    修改时间显示，删除空格

--- statusbar/packages/date.sh
+++ statusbar/packages/date.sh
@@ -13,7 +13,7 @@ signal=$(echo "^s$this^" | sed 's/_//')
 time_icon=" "
 
 update() {
-    time_text="$(date '+%b%e 周%a %H:%M')"
+    time_text="$(date '+%b%-d日 周%a %H:%M')"
     case "$(date '+%I')" in
         "01") time_icon="" ;;
         "02") time_icon="" ;;
