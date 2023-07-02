f51439ec95d4b763b1430a610dbe919149cc5eb5
Lxw0628 <761402785@qq.com>
2023-07-03 00:34:00 +0800

    update

--- autostart.sh
+++ autostart.sh
@@ -6,7 +6,7 @@ killall picom
 killall flameshot
 killall udiskie
 killall fcitx5
-# killall cfw
+killall cfw
 killall blueman-applet
 killall nm-applet
 killall dunst
@@ -28,7 +28,7 @@ nm-applet &
 blueman-applet &
 # bluetoothctl power off
 
-# /opt/clash-for-windows-chinese/cfw &
+/opt/clash-for-windows-chinese/cfw &
 
 fcitx5 &
 
@@ -42,8 +42,5 @@ feh --bg-fill --randomize $HOME/Pictures/wallpapers/* &
 # feh --bg-fill --randomize $HOME/Pictures/wallpapers/8c30a659880811ebb6edd017c2d2eca2.png &
 
 /usr/lib/xfce-polkit/xfce-polkit &
-
-xmodmap -e "keycode 108 = Super_L"  # reassign Alt_R to Super_L
-xmodmap -e "remove mod1 = Super_L"  # make sure X keeps it out of the mod1 group
 
 xset r rate 250 50
