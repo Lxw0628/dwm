55c083ab3c8f73057ac182a7077c82742b17e6e8
Lxw0628 <761402785@qq.com>
2024-09-07 19:17:44 +0800

    format shiftview.c

--- shiftview.c
+++ shiftview.c
@@ -11,14 +11,16 @@
 void
 shiftview(const Arg *arg) {
   Arg shifted;
- 
-  if(arg->i > 0) // left circular shift
-    shifted.ui = (selmon->tagset[selmon->seltags] << arg->i)
-      | (selmon->tagset[selmon->seltags] >> (LENGTH(tags) - arg->i));
- 
-  else // right circular shift
-    shifted.ui = selmon->tagset[selmon->seltags] >> (- arg->i)
-      | selmon->tagset[selmon->seltags] << (LENGTH(tags) + arg->i);
- 
+
+  // left circular shift
+  if (arg->i > 0)
+    shifted.ui = (selmon->tagset[selmon->seltags] << arg->i) |
+                 (selmon->tagset[selmon->seltags] >> (LENGTH(tags) - arg->i));
+
+  // right circular shift
+  else
+    shifted.ui = selmon->tagset[selmon->seltags] >> (-arg->i) |
+                 selmon->tagset[selmon->seltags] << (LENGTH(tags) + arg->i);
+
   view(&shifted);
 }
