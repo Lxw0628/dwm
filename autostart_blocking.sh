fcc7c0f308fe7872d72eea66e8a8a1c66bb2835b
Lxw0628 <761402785@qq.com>
2024-10-18 17:11:51 +0800

    启动dwm时只显示外接显示器 --output eDP-1 --off

--- autostart_blocking.sh
+++ autostart_blocking.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-# xrandr --output HDMI-2 --mode 1920x1080 --rate 120.00 --output eDP-1 --off
-xrandr --output HDMI-2 --mode 1920x1080 --rate 120.00 --output eDP-1 --mode 1920x1080 --rate 60.00 --right-of HDMI-2
+xrandr --output HDMI-2 --mode 1920x1080 --rate 120.00 --output eDP-1 --off
+
 
 # killall -q bar.sh
 sh ~/.config/dwm/scripts/bar.sh &
