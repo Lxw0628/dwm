9b450e416471ce81eab5b2b475dc5e1037fdc72b
Lxw0628 <761402785@qq.com>
2024-11-05 12:27:25 +0800

    terminal -> kitty

--- config.def.h
+++ config.def.h
@@ -158,6 +158,7 @@ static const Layout layouts[] = {
 #define SHCMD(cmd) { .v = (const char*[]){ "/bin/sh", "-c", cmd, NULL } }
 
 /* commands */
+static const char *termcmd[]  = { "kitty", NULL };
 static const char *upvol[]            = { "/usr/bin/pactl", "set-sink-volume", "0", "+5%",     NULL };
 static const char *downvol[]          = { "/usr/bin/pactl", "set-sink-volume", "0", "-5%",     NULL };
 static const char *mutevol[]          = { "/usr/bin/pactl", "set-sink-mute",   "0", "toggle",  NULL };
