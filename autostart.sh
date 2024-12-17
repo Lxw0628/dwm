61cbdee73b3ed59238a775c5a847ad844f53a225
Lxw0628 <761402785@qq.com>
2024-12-18 02:29:30 +0800

    fix: 删除autostart_blocking.sh并将其内容移动到autostart.sh; 使用pkill杀死bar.sh进程在每次dwm restart时

--- autostart.sh
+++ autostart.sh
@@ -4,6 +4,7 @@
 export LANG=zh_CN.UTF-8
 export LANGUAGE=zh_CN:en_US
 
+xrandr --output HDMI-2 --mode 1920x1080 --rate 120.00 --output eDP-1 --off
 
 numlockx on
 # ulimit -n 打开的文件描述符的最大个数
@@ -16,6 +17,8 @@ feh --bg-fill --no-fehbg ~/Pictures/wallpapers/catppuccin/mocha.png
 picom --config ~/.config/picom/picom.conf &
 # picom --experimental-backends --config ~/.config/picom/picom.conf &
 
+pkill -f /home/lxw0628/.config/dwm/scripts/bar.sh
+sh ~/.config/dwm/scripts/bar.sh &
 
 fcitx5 -d &
 blueman-applet &
--- autostart_blocking.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-xrandr --output HDMI-2 --mode 1920x1080 --rate 120.00 --output eDP-1 --off
-
-
-# killall -q bar.sh
-sh ~/.config/dwm/scripts/bar.sh &
