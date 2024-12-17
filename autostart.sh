5dfcbcaca63222f169b2607dc991514066290b33
Lxw0628 <761402785@qq.com>
2024-12-18 02:46:38 +0800

    style: 调整顺序

--- autostart.sh
+++ autostart.sh
@@ -1,9 +1,5 @@
 #!/bin/bash
 
-
-export LANG=zh_CN.UTF-8
-export LANGUAGE=zh_CN:en_US
-
 xrandr --output HDMI-2 --mode 1920x1080 --rate 120.00 --output eDP-1 --off
 
 numlockx on
@@ -11,26 +7,25 @@ numlockx on
 ulimit -n 4096
 xrdb merge ~/.Xresources
 xset r rate 200 50
-# xbacklight -set 10 &
-# feh --bg-fill --no-fehbg ~/Pictures/wallpapers/osu_bg/BG.jpg
 feh --bg-fill --no-fehbg ~/Pictures/wallpapers/catppuccin/mocha.png
-picom --config ~/.config/picom/picom.conf &
-# picom --experimental-backends --config ~/.config/picom/picom.conf &
+# picom --config ~/.config/picom/picom.conf &
+picom --experimental-backends --config ~/.config/picom/picom.conf &
 
 pkill -f /home/lxw0628/.config/dwm/scripts/bar.sh
 sh ~/.config/dwm/scripts/bar.sh &
 
+/usr/lib/polkit-gnome/polkit-gnome-authentication-agent-1 &
+/usr/bin/dunst &
 fcitx5 -d &
 blueman-applet &
 pa-applet &
 nm-applet &
 birdtray &
 udiskie --tray &
-clash-verge &
-/usr/lib/polkit-gnome/polkit-gnome-authentication-agent-1 &
-/usr/bin/dunst &
 thunar --daemon &
 xsettingsd &
 xfce4-power-manager &
 
 Snipaste &
+clash-verge &
+otd-daemon &
