a77818df0d8fdeb79941538b5da60311b8acfd21
Lxw0628 <761402785@qq.com>
2024-12-30 01:14:11 +0800

    flamingo=>mauve

--- scripts/bar.sh
+++ scripts/bar.sh
@@ -6,19 +6,19 @@
 interval=0
 
 # load colors
-. ~/.config/dwm/scripts/bar_themes/catppuccin
+. ~/.config/dwm/statusbar/bar_themes/catppuccin
 
 disk() {
   disk_can_use_kb="$(df | sed -n '5p' | awk '{print $4}')"
   disk_can_use_gb=$(echo "scale=2; $disk_can_use_kb/1024/1024" | bc)
-  printf "^c$flamingo^ /:"
+  printf "^c$mauve^ /:"
   printf "^c$text^ $disk_can_use_gb GB"
 }
 
 cpu() {
   # cpu_val=$(grep -o "^[^ ]*" /proc/loadavg)
   cpu_val=$(top -n 1 -b | sed -n '3p' | awk '{printf "%02d%", 100 - $8}')
-  printf "^c$flamingo^ CPU:"
+  printf "^c$mauve^ CPU:"
   printf "^c$text^ $cpu_val%"
 }
 
@@ -28,7 +28,7 @@ battery() {
 }
 
 brightness() {
-  printf "^c$flamingo^ Bri:"
+  printf "^c$mauve^ Bri:"
   # printf "^c$text^%.0f\n" $(cat /sys/class/backlight/*/brightness)
   printf "^c$text^%.0f\n" $(ddcutil getvcp 12 | awk '{print $9}' | awk -F, '{print $1}')
 }
@@ -39,20 +39,20 @@ mem() {
   mem_used_kb=$((mem_total_kb - mem_free_kb))
   mem_used_gb=$(echo "scale=2; $mem_used_kb/1024/1024" | bc)
   # printf "^c$text^^b$black^  "
-  printf "^c$flamingo^ Mem:"
+  printf "^c$mauve^ Mem:"
   printf "^c$text^ $mem_used_gb GB"
 }
 
 wlan() {
 	case "$(cat /sys/class/net/wl*/operstate 2>/dev/null)" in
-	up) printf "^c$flamingo^ 󰤨  ^d^%s" " ^c$text^Connected" ;;
-	down) printf "^c$flamingo^ 󰤭  ^d^%s" " ^c$text^Disconnected" ;;
+	up) printf "^c$mauve^ 󰤨  ^d^%s" " ^c$text^Connected" ;;
+	down) printf "^c$mauve^ 󰤭  ^d^%s" " ^c$text^Disconnected" ;;
 	esac
 }
 
 clock() {
 	# printf "^c$text^ 󱑆  "
-	printf "^c$flamingo^ $(date '+%Y-%m-%d %H:%M') "
+	printf "^c$mauve^ $(date '+%Y-%m-%d %H:%M') "
 }
 
 while true; do
